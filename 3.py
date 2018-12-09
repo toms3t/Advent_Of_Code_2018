d478b47b0cb601e73635c9cbbc6f89038e180080
Tom Setliffe <tomset@gmail.com>
2018-12-09 10:39:46 -0800

    Day 3

--- 3.py
+++ 3.py
@@ -1,55 +1,22 @@
-# 1. Parse claims file and use claim details to create data structure
-#    Data structure = dictionary -- (ID: [x,y,width,height]
-#    or.... uses classes ID.x, ID.y, ID.width, ID.height
-# 2. Determine the largest dimension of fabric needed to fit all claims
-# 3. Assign a tuple of x,y values for each spot on the matrix as class variable
-# 4. Define instance methods - create_claim_dict, draw 2d matrix,
-#    draw claim on matrix, and calculate_overlap
-
-
-# NO NEED TO DRAW THE 2D MATRIX
-# MATRIX is 1000 sq inches (no need to calc the fabric size)
-# Next step, draw 1 claim on matrix and imprint size of claim with claim num
 import re
 
 
-class Fabric:
+class Claim:
 	def __init__(self, num, x, y, width, height):
 		self.num = num
 		self.x = int(x)
 		self.y = int(y)
 		self.width = int(width)
 		self.height = int(height)
+		self.planned_claim_area = int(width)*int(height)
+		Claim.CLAIM_OVERLAP_DICT[self.num] = [self.planned_claim_area]
 
-	with open('claims', 'r') as f:
-		CLAIMS = f.readlines()
+	with open('claim_list', 'r') as f:
+		CLAIM_LIST = f.readlines()
 
-	# MAX_X = 0
-	# MAX_Y = 0
 	MATRIX = [['.' for _ in range(1000)] for _ in range(1000)]
-
-	# def find_matrix_size(self):
-	# 	if self.x+self.width > Fabric.MAX_X:
-	# 		Fabric.MAX_X = self.x + self.width
-	# 	if self.y+self.height > Fabric.MAX_Y:
-	# 		Fabric.MAX_Y = self.y + self.height
-	#
-	# @classmethod
-	# def create_2d_matrix(cls):
-	# 	for _ in range(cls.MAX_Y):
-	# 		Fabric.MATRIX.append(['.' * cls.MAX_X])
-	#
-	# @classmethod
-	# def draw_2d_matrix(cls):
-	# 	for row in cls.MATRIX:
-	# 		print(row)
-
-
-	def draw_claim_on_matrix(self):
-		pass
-
-	def calculate_overlap(self):
-		pass
+	OVERLAP_COUNT = 0
+	CLAIM_OVERLAP_DICT = {}
 
 	@classmethod
 	def from_file(cls, claim_str):
@@ -58,31 +25,48 @@ class Fabric:
 			regex_pattern.match(claim_str).group(1, 2, 3, 4, 5)
 		return cls(num, x, y, width, height)
 
-X = 0
-for claim in Fabric.CLAIMS:
-	claimObj = Fabric.from_file(claim)
-	x = claimObj.x
-	y = claimObj.y
-	y_incrementer = 0
-	for _ in range(claimObj.height):
-		x_incrementer = 0
-		for _ in range(claimObj.width):
-			try:
-				if claimObj.MATRIX[y + y_incrementer][x + x_incrementer] == '#':
-					claimObj.MATRIX[y + y_incrementer][x + x_incrementer] = 'X'
-					X += 1
-				elif claimObj.MATRIX[y + y_incrementer][x + x_incrementer] == '.':
-					claimObj.MATRIX[y + y_incrementer][x + x_incrementer] = '#'
-			except IndexError:
-				continue
-			x_incrementer += 1
-		y_incrementer += 1
+	def overlap_counter(self, _id):
+		y_incrementer = 0
+		for _ in range(claimObj.height):
+			x_incrementer = 0
+			for _ in range(claimObj.width):
+				try:
+					if claimObj.MATRIX[self.y + y_incrementer][
+						self.x + x_incrementer].isdigit():
+						claimObj.MATRIX[self.y + y_incrementer][
+							self.x + x_incrementer] = 'X'
+						Claim.OVERLAP_COUNT += 1
+					elif claimObj.MATRIX[self.y + y_incrementer][
+						self.x + x_incrementer] == '.':
+						claimObj.MATRIX[self.y + y_incrementer][
+							self.x + x_incrementer] = claimObj.num
+				except IndexError:
+					continue
+				x_incrementer += 1
+			y_incrementer += 1
+
+	@classmethod
+	def complete_claim_finder(cls, _id):
+		count = 0
+		for row in Claim.MATRIX:
+			for inch in row:
+				if inch == _id:
+					count += 1
+		Claim.CLAIM_OVERLAP_DICT[_id].append(count)
+		if Claim.CLAIM_OVERLAP_DICT[_id][0] == Claim.CLAIM_OVERLAP_DICT[_id][1]:
+			return _id
 
 
-print('X', X)
+for _id in Claim.CLAIM_LIST:
+	claimObj = Claim.from_file(_id)
+	claimObj.overlap_counter(_id)
 
+non_overlap_claim = ''
+for claim in Claim.CLAIM_OVERLAP_DICT:
+	if Claim.complete_claim_finder(claim):
+		non_overlap_claim = Claim.complete_claim_finder(claim)
 
+print('Part 1 answer is: {}'.format(Claim.OVERLAP_COUNT))
+print('Part 2 answer is: {}'.format(non_overlap_claim))
 
-
-#1 @ 432,394: 29x14
 
