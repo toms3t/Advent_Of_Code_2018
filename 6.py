818daea873ab1762f775c79a89b7741cfe145195
Tom Setliffe <tomset@gmail.com>
2018-12-18 14:22:03 -0800

    Day 6 Start

---
+++ 6.py
@@ -0,0 +1,33 @@
+# functions
+
+# read in coordinates into list of tuples
+# Draw map (coordinates and proximity letters) - also use this to draw basic map using max coordinates
+# apply Manhattan distance
+# find largest area
+
+# from itertools import product
+# coordinates = list(product(xrange(width), xrange(height)))
+
+class ChronalMap:
+
+	def __init__(self):
+		pass
+
+	MAP = []
+
+	def read_in_coordinates(self):
+		pass
+
+	def draw_map(self):
+		pass
+
+	def calc_manhattan_distance(self):
+		pass
+
+	def find_largest_area(self):
+		pass
+
+
+
+
+
